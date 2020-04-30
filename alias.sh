bda7db46fb18ea2708b53166ecf431b71d727cd3
David Castaneda <dcast188@fiu.edu>
2020-04-30 16:18:25 -0400

    updated

--- alias.sh
+++ alias.sh
@@ -1,10 +1,14 @@
-# github
+# github alias
 alias g="git"
 alias gc="git commit"
 alias ga="git add"
 alias gp="git push"
 
-# k8s
+# k8s alias
 alias k="kubectl"
 
-# kafka
+# vault alias
+alias v="vault"
+
+# General aliases
+alias home="cd ~/"
